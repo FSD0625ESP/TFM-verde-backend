d052be6f8d6371407e60f6119f3d93988655403e
Gerard <llanasnas@gmail.com>
2025-10-24 19:36:22 +0200

    fix typo

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: "http://localhost:5174", // Cambia esto a la URL de tu frontend
-    credentials: true, // Habilita el envío de cookies
-  })
+    cors({
+        origin: "http://localhost:5174", // Cambia esto a la URL de tu frontend
+        credentials: true, // Habilita el envío de cookies
+    })
 );
 
 require("dotenv").config();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.use("/users", userRoutes);
@@ -32,5 +32,5 @@ app.use("/stores", storeRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });

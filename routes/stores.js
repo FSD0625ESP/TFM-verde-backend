ce5bc7ae8739abc8f0cae11c2ea34653ec383895
llanasnas <32850289+llanasnas@users.noreply.github.com>
2025-11-28 17:37:31 +0100

    resolved conflicts

--- routes/stores.js
+++ routes/stores.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const router = express.Router();
-const { getAllStores, registerStore, searchStores } = require("../controllers/storeController");
-
-router.get("/all", getAllStores);
-router.get("/search", searchStores);
 const {
   getAllStores,
   getStoreById,
   getStoreBySellerId,
   registerStore,
+  searchStores,
 } = require("../controllers/storeController");
+const router = express.Router();
 
 router.get("/all", getAllStores);
+router.get("/search", searchStores);
+
 router.get("/store/:id", getStoreById);
 router.get("/store/seller/:id", getStoreBySellerId);
 router.post("/register", registerStore);

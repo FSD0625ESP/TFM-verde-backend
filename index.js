52fd6e364815ee1a517eea2f4284cf707058bd3c
llanasnas <32850289+llanasnas@users.noreply.github.com>
2026-01-23 19:24:49 +0100

    add cors prod url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://meraki-frontend.netlify.app"],
     credentials: true,
   })
 );
